033ea491152949ee0f3bfcd483306fc0e6e12eb7
eeexuanlyx <eeexuanlyx@gmail.com>
2024-10-04 17:34:04 +0800

    add start, pause, reset

--- app.js
+++ app.js
@@ -5,21 +5,86 @@ const messageElement = document.querySelector("#message");
 const baitElement = document.querySelector("#bait");
 const timerElement = document.querySelector("#timer");
 const startButtonElement = document.querySelector(".button-start");
-const stopButtonElement = document.querySelector(".button-stop");
+const pauseButtonElement = document.querySelector(".button-pause");
 const resetButtonElement = document.querySelector(".button-reset");
 
 /*---------- Variables (state) ---------*/
-let lastHole;
 let score = 0;
 let message = "";
 let bait = "10";
 let timeUp = false;
 let currentHole = "";
+let gameStarted = false;
+let gameOver = false;
 /*----- Cached Element References  -----*/
 
 /*-------------- Functions -------------*/
+
+function init() {
+  score = 0;
+  message = "";
+  bait = 10;
+  timeUp = false;
+  currentHole = "";
+  gameStarted = false;
+  gameOver = false;
+  render();
+  messageElement.textContent = "Loading Complete, begin catching!";
+  baitElement.textContent = 10;
+  scoreElement.textContent = 0;
+}
+
+function render() {
+  updateMessage();
+}
+
+function updateMessage() {
+  if (bait === 0 && score < 5) {
+    gameOver = true;
+    gameStarted = false;
+    messageElement.textContent = "Game Over, you lose!";
+  }
+  if (score > 4) {
+    gameOver = true;
+    gameStarted = false;
+    messageElement.textContent = "Game Over, you win!";
+  }
+}
+
 function startGame() {
-  fishAppears();
+  if (gameStarted === false) {
+    init();
+    gameStarted = true;
+    fishAppears();
+  }
+  if (gameOver === true && gameStarted === false) {
+    //if game not started, then the start button can be clicked
+    init();
+    gameStarted = true;
+    fishAppears();
+  }
+  console.log("game over:" + gameOver);
+  console.log("game started:" + gameStarted);
+}
+
+function resetGame() {
+  init();
+  gameOver = true;
+}
+
+function pauseGame() {
+  switch (pauseButtonElement.textContent) {
+    case "Pause":
+      gameOver = true; //to pause fishes from coming out
+      pauseButtonElement.textContent = "Resume";
+      break;
+    case "Resume":
+      gameOver = false;
+      // to spawn the fish again
+      fishAppears();
+      pauseButtonElement.textContent = "Pause";
+      break;
+  }
 }
 
 function fishAppears() {
@@ -28,7 +93,7 @@ function fishAppears() {
   holesElement[idx].classList.add("fish");
   setTimeout(() => {
     holesElement[idx].classList.remove("fish");
-    if (!timeUp) fishAppears();
+    if (!timeUp && gameOver === false) fishAppears();
   }, time);
 }
 
@@ -37,12 +102,17 @@ function randomTime(min, max) {
 }
 
 function handleFishClick(e) {
-  bait -= 1;
-  baitElement.textContent = bait;
-  if (e.target.classList.contains("fish")) {
-    score += 1;
-    this.classList.remove("fish");
-    scoreElement.textContent = score;
+  if (gameOver === false) {
+    if (bait !== 0) {
+      bait -= 1;
+    }
+    baitElement.textContent = bait;
+    if (e.target.classList.contains("fish")) {
+      score += 1;
+      this.classList.remove("fish");
+      scoreElement.textContent = score;
+    }
+    render();
   }
 }
 
@@ -53,5 +123,8 @@ function checkScore() {
 }
 
 /*----------- Event Listeners ----------*/
+init();
 holesElement.forEach((hole) => hole.addEventListener("click", handleFishClick));
 startButtonElement.addEventListener("click", startGame);
+pauseButtonElement.addEventListener("click", pauseGame);
+resetButtonElement.addEventListener("click", resetGame);
